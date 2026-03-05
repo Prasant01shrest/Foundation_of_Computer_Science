3b24e96e202b94b4a84c5e70f83a6005866ed3f9
Prasant Shrestha <zeno7291@gmail.com>
2026-03-05 19:42:53 +0545

    Implement seating arrangement validation with permutations

---
+++ Task 2/brute.py
@@ -0,0 +1,32 @@
+import itertools
+
+students = ["A", "B", "C", "D"]
+
+friends = [("A", "B")]
+cities = {
+    "A": "KTM",
+    "B": "PKR",
+    "C": "KTM",
+    "D": "BRT"
+}
+
+for arrangement in itertools.permutations(students):
+    valid = True
+
+    for i in range(len(arrangement) - 1):
+        s1 = arrangement[i]
+        s2 = arrangement[i + 1]
+
+        if (s1, s2) in friends or (s2, s1) in friends:
+            valid = False
+
+        if cities[s1] == cities[s2]:
+            valid = False
+
+    if valid:
+        print("Valid seating:", arrangement)
+PS E:\Python Class> python brute.py
+Valid seating: ('A', 'D', 'B', 'C')
+Valid seating: ('A', 'D', 'C', 'B')
+Valid seating: ('B', 'C', 'D', 'A')
+Valid seating: ('C', 'B', 'D', 'A')
