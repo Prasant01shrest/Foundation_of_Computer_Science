26d375cbd494951d5583318e5ef7700a6c45db5a
Prasant Shrestha <zeno7291@gmail.com>
2026-03-05 19:44:04 +0545

    Add heuristic seating algorithm in heu.py
    Implement heuristic seating arrangement based on friends and cities.

---
+++ Task 2/heu.py
@@ -0,0 +1,21 @@
+students = ["A", "B", "C", "D"]
+
+friends = [("A","B"), ("B","A")]
+
+cities = {"A":"KTM", "B":"PKR", "C":"KTM", "D":"BRT"}
+
+seating = []
+
+for s in students:
+    if not seating:
+        seating.append(s)
+    else:
+        last = seating[-1]
+        
+        if (last, s) not in friends and cities[last] != cities[s]:
+            seating.append(s)
+
+print("Heuristic seating:", seating)
+
+PS E:\Python Class> python heu.py
+Heuristic seating: ['A', 'D']
